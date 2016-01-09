7ed9f4098e19d9dd82a48b70dda6c94a5a7c1a50
uNkNowN92 <christianjohnsumampong@gmail.com>
2016-01-09 23:44:59 +0800

    Moved unused codes to bottom

--- PagedListDemo/Common/HtmlHelpers.cs
+++ PagedListDemo/Common/HtmlHelpers.cs
@@ -32,79 +32,6 @@ namespace PagedListDemo.Common
             return jsonPropertyName;
         }
 
-        //public static MvcHtmlString RadioButtonToggleFor<TModel, TProperty>(this HtmlHelper<TModel> helper, Expression<Func<TModel, TProperty>> expression)
-        //{
-        //    var data = ModelMetadata.FromLambdaExpression(expression, helper.ViewData);
-
-        //    Func<TModel, TProperty> method = expression.Compile();
-        //    bool value = Convert.ToBoolean(method(helper.ViewData.Model));
-
-        //    IDictionary<string, object> attrs = HtmlHelper.AnonymousObjectToHtmlAttributes(new { });
-        //    IDictionary<string, object> defaultDivAttrs = HtmlHelper.AnonymousObjectToHtmlAttributes(_radioButtonToggleDivDefaultAttrs);
-        //    IDictionary<string, object> buttonLabel = HtmlHelper.AnonymousObjectToHtmlAttributes(_radioButtonToggleDefaultLabel);
-        //    IDictionary<string, object> labelTrueAttrs = HtmlHelper.AnonymousObjectToHtmlAttributes(_radioButtonToggleDefaultAttrs);
-        //    IDictionary<string, object> labelFalseAttrs = HtmlHelper.AnonymousObjectToHtmlAttributes(_radioButtonToggleDefaultAttrs);
-
-        //    buttonLabel["labelTrue"] = buttonLabel["labelTrue"];
-        //    buttonLabel["labelFalse"] = buttonLabel["labelFalse"];
-
-        //    attrs["class"] = defaultDivAttrs["class"];
-        //    attrs["data-toggle"] = defaultDivAttrs["data-toggle"] + (attrs.Keys.Contains("data-toggle") ? " " + attrs["data-toggle"] : "");
-
-        //    string radioTrueActive = (value ? " active" : "") + " radio-button-toggle-true" + " btn-default";
-        //    string radioFalseActive = (!value ? " active" : "") + " radio-button-toggle-false" + " btn-default";
-
-        //    MvcHtmlString radioTrue = helper.RadioButton(data.PropertyName, true, value,
-        //        new { id = data.PropertyName + (string.IsNullOrEmpty(Convert.ToString(buttonLabel["labelTrue"])) ? "True" : buttonLabel["labelTrue"]) });
-        //    MvcHtmlString radioFalse = helper.RadioButton(data.PropertyName, false, !value,
-        //        new { id = data.PropertyName + (string.IsNullOrEmpty(Convert.ToString(buttonLabel["labelFalse"])) ? "False" : buttonLabel["labelFalse"]) });
-
-        //    TagBuilder div = new TagBuilder("div");
-        //    div.MergeAttributes(attrs);
-
-        //    TagBuilder labelTrue = new TagBuilder("label");
-
-        //    labelTrueAttrs["class"] += radioTrueActive;
-        //    labelTrue.MergeAttributes(labelTrueAttrs);
-        //    labelTrue.InnerHtml += radioTrue;
-        //    labelTrue.InnerHtml += buttonLabel["labelTrue"];
-
-        //    TagBuilder labelFalse = new TagBuilder("label");
-
-        //    labelFalseAttrs["class"] += radioFalseActive;
-        //    labelFalse.MergeAttributes(labelFalseAttrs);
-        //    labelFalse.InnerHtml += radioFalse;
-        //    labelFalse.InnerHtml += buttonLabel["labelFalse"];
-
-        //    div.InnerHtml += labelTrue;
-        //    div.InnerHtml += labelFalse;
-
-        //    return new MvcHtmlString(div.ToString(TagRenderMode.Normal));
-        //    //return helper.RadioButtonToggleFor(expression, new {});
-        //    //_radioButtonToggleTemplate = @"<div class='radio-button-toggle btn-group' data-toggle='buttons'>
-        //    //    <label class='btn btn-default{0}'>
-        //    //        {1}Yes
-        //    //    </label>
-        //    //    <label class='btn btn-default{2}'>
-        //    //        {3}No
-        //    //    </label>
-        //    //</div>";
-
-        //    //var data = ModelMetadata.FromLambdaExpression(expression, helper.ViewData);
-
-        //    //Func<TModel, TProperty> method = expression.Compile();
-        //    //bool value = Convert.ToBoolean(method(helper.ViewData.Model));
-
-        //    //string radioTrueActive = value ? " active" : "";
-        //    //string radioFalseActive = !value ? " active" : "";
-        //    //MvcHtmlString radioTrue = helper.RadioButton(data.PropertyName, true, value);
-        //    //MvcHtmlString radioFalse = helper.RadioButton(data.PropertyName, false, !value);
-
-        //    //var result = string.Format(_radioButtonToggleTemplate, radioTrueActive, radioTrue, radioFalseActive, radioFalse);
-
-        //    //return new MvcHtmlString(result);
-        //}
-
         private static object _radioButtonToggleDivDefaultAttrs = new
         {
             @class = "radio-button-toggle btn-group",
@@ -215,15 +142,7 @@ namespace PagedListDemo.Common
             var buttonLabel = _buttonLabel.Concat(toggleButtons)
                 .GroupBy(kvp => kvp.Key, kvp => kvp.Value)
                 .ToDictionary(g => g.Key, g => g.LastOrDefault());
-
-            //buttonLabel["labelTrue"] = toggleButtons["labelTrue"] ?? buttonLabel["labelTrue"];
-            //buttonLabel["labelFalse"] = toggleButtons["labelFalse"] ?? buttonLabel["labelFalse"];
-            //labelTrueAttrs["class"] = labelTrueAttrs["class"] + (toggleButtons.Keys.Contains("class") ? " " + toggleButtons["class"] : "");
-            //labelFalseAttrs["class"] = labelFalseAttrs["class"] + (toggleButtons.Keys.Contains("class") ? " " + toggleButtons["class"] : "");
-
-            //attrs["class"] = defaultDivAttrs["class"] + (attrs.Keys.Contains("class") ? " " + attrs["class"] : "");
-            //attrs["data-toggle"] = defaultDivAttrs["data-toggle"] + (attrs.Keys.Contains("data-toggle") ? " " + attrs["data-toggle"] : "");
-
+            
             string radioTrueActive = (value ? " active" : "") + " radio-button-toggle-true";
             string radioFalseActive = (!value ? " active" : "") + " radio-button-toggle-false";
             string toggleTrueId = data.PropertyName + (string.IsNullOrEmpty(Convert.ToString(buttonLabel["labelTrue"])) ? "True" : buttonLabel["labelTrue"]);
@@ -256,6 +175,79 @@ namespace PagedListDemo.Common
 
             return new MvcHtmlString(div.ToString(TagRenderMode.Normal));
         }
+
+        //public static MvcHtmlString RadioButtonToggleFor<TModel, TProperty>(this HtmlHelper<TModel> helper, Expression<Func<TModel, TProperty>> expression)
+        //{
+        //    var data = ModelMetadata.FromLambdaExpression(expression, helper.ViewData);
+
+        //    Func<TModel, TProperty> method = expression.Compile();
+        //    bool value = Convert.ToBoolean(method(helper.ViewData.Model));
+
+        //    IDictionary<string, object> attrs = HtmlHelper.AnonymousObjectToHtmlAttributes(new { });
+        //    IDictionary<string, object> defaultDivAttrs = HtmlHelper.AnonymousObjectToHtmlAttributes(_radioButtonToggleDivDefaultAttrs);
+        //    IDictionary<string, object> buttonLabel = HtmlHelper.AnonymousObjectToHtmlAttributes(_radioButtonToggleDefaultLabel);
+        //    IDictionary<string, object> labelTrueAttrs = HtmlHelper.AnonymousObjectToHtmlAttributes(_radioButtonToggleDefaultAttrs);
+        //    IDictionary<string, object> labelFalseAttrs = HtmlHelper.AnonymousObjectToHtmlAttributes(_radioButtonToggleDefaultAttrs);
+
+        //    buttonLabel["labelTrue"] = buttonLabel["labelTrue"];
+        //    buttonLabel["labelFalse"] = buttonLabel["labelFalse"];
+
+        //    attrs["class"] = defaultDivAttrs["class"];
+        //    attrs["data-toggle"] = defaultDivAttrs["data-toggle"] + (attrs.Keys.Contains("data-toggle") ? " " + attrs["data-toggle"] : "");
+
+        //    string radioTrueActive = (value ? " active" : "") + " radio-button-toggle-true" + " btn-default";
+        //    string radioFalseActive = (!value ? " active" : "") + " radio-button-toggle-false" + " btn-default";
+
+        //    MvcHtmlString radioTrue = helper.RadioButton(data.PropertyName, true, value,
+        //        new { id = data.PropertyName + (string.IsNullOrEmpty(Convert.ToString(buttonLabel["labelTrue"])) ? "True" : buttonLabel["labelTrue"]) });
+        //    MvcHtmlString radioFalse = helper.RadioButton(data.PropertyName, false, !value,
+        //        new { id = data.PropertyName + (string.IsNullOrEmpty(Convert.ToString(buttonLabel["labelFalse"])) ? "False" : buttonLabel["labelFalse"]) });
+
+        //    TagBuilder div = new TagBuilder("div");
+        //    div.MergeAttributes(attrs);
+
+        //    TagBuilder labelTrue = new TagBuilder("label");
+
+        //    labelTrueAttrs["class"] += radioTrueActive;
+        //    labelTrue.MergeAttributes(labelTrueAttrs);
+        //    labelTrue.InnerHtml += radioTrue;
+        //    labelTrue.InnerHtml += buttonLabel["labelTrue"];
+
+        //    TagBuilder labelFalse = new TagBuilder("label");
+
+        //    labelFalseAttrs["class"] += radioFalseActive;
+        //    labelFalse.MergeAttributes(labelFalseAttrs);
+        //    labelFalse.InnerHtml += radioFalse;
+        //    labelFalse.InnerHtml += buttonLabel["labelFalse"];
+
+        //    div.InnerHtml += labelTrue;
+        //    div.InnerHtml += labelFalse;
+
+        //    return new MvcHtmlString(div.ToString(TagRenderMode.Normal));
+        //    //return helper.RadioButtonToggleFor(expression, new {});
+        //    //_radioButtonToggleTemplate = @"<div class='radio-button-toggle btn-group' data-toggle='buttons'>
+        //    //    <label class='btn btn-default{0}'>
+        //    //        {1}Yes
+        //    //    </label>
+        //    //    <label class='btn btn-default{2}'>
+        //    //        {3}No
+        //    //    </label>
+        //    //</div>";
+
+        //    //var data = ModelMetadata.FromLambdaExpression(expression, helper.ViewData);
+
+        //    //Func<TModel, TProperty> method = expression.Compile();
+        //    //bool value = Convert.ToBoolean(method(helper.ViewData.Model));
+
+        //    //string radioTrueActive = value ? " active" : "";
+        //    //string radioFalseActive = !value ? " active" : "";
+        //    //MvcHtmlString radioTrue = helper.RadioButton(data.PropertyName, true, value);
+        //    //MvcHtmlString radioFalse = helper.RadioButton(data.PropertyName, false, !value);
+
+        //    //var result = string.Format(_radioButtonToggleTemplate, radioTrueActive, radioTrue, radioFalseActive, radioFalse);
+
+        //    //return new MvcHtmlString(result);
+        //}
     }
 
     public class HtmlHelperActionInvoker
