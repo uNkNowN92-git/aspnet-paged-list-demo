0d34b2661ad6ca52780717f47da38c0449ed36a7
Christian John Sumampong <uNkNowN92-git@users.noreply.github.com>
2016-01-24 15:17:17 +0800

    Update MvcHelper.cs

--- PagedListDemo/Common/MvcHelper.cs
+++ PagedListDemo/Common/MvcHelper.cs
@@ -11,13 +11,14 @@ namespace PagedListDemo.Common
 	public static class MvcHelper
 	{
 		public const string contentPathTemplate = "~/Content/{0}";
-		public const string bundlesPathTemplate = "~/bundles/{0}";
 		public const string scriptsPathTemplate = "~/Scripts/{0}";
+		public const string bundlesPathTemplate = "~/bundles/{0}";
+		public const string stylesPathTemplate = "~/Content/{0}Styles";
 		
 		public static MvcHtmlString RenderControllerStyleBundle(this HtmlHelper helper)
 		{
 			var controllerName = helper.ViewContext.RouteData.Values["controller"];
-			var bundle = BundleTable.Bundles.GetBundleFor(string.Format(contentPathTemplate, controllerName));
+			var bundle = BundleTable.Bundles.GetBundleFor(string.Format(stylesPathTemplate, controllerName));
 			
 			if (bundle != null)
 				return new MvcHtmlString(Styles.Render(bundle.Path).ToString());
@@ -42,17 +43,18 @@ namespace PagedListDemo.Common
 				var contentPath = string.Format(MvcHelper.contentPathTemplate, controllerName);
 				var scriptsPath = string.Format(MvcHelper.scriptsPathTemplate, controllerName);
 				var bundlesPath = string.Format(MvcHelper.bundlesPathTemplate, controllerName);
+				var stylesPath = string.Format(MvcHelper.stylesPathTemplate, controllerName);
 			
 				var contentServerPath = HttpContext.Current.Server.MapPath(contentPath);
 				var scriptsServerPath = HttpContext.Current.Server.MapPath(scriptsPath);
 			
 				if (System.IO.Directory.Exists(contentServerPath))
-					bundles.Add(new StyleBundle(contentPath)
-	            .Include(string.Format("{0}/*.css", contentPath)));
+					bundles.Add(new StyleBundle(stylesPath).Include(
+						string.Format("{0}/*.css", contentPath)));
 			
 				if (System.IO.Directory.Exists(scriptsServerPath))
-					bundles.Add(new ScriptBundle(bundlesPath)
-	            .Include(string.Format("{0}/*.js", scriptsPath)));
+					bundles.Add(new ScriptBundle(bundlesPath).Include(
+						string.Format("{0}/*.js", scriptsPath)));
 			});
 		}
 		
