c894cd532eddef516ed669c22716ce4aa1a5c84e
uNkNowN92 <christianjohnsumampong@gmail.com>
2016-01-25 23:47:07 +0800

    Added test for MustBeTrueAttribute

---
+++ PagedListDemoTests/Common/MustBeTrueAttributeTests.cs
@@ -0,0 +1,67 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using PagedListDemo.Common;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PagedListDemo.Common.Tests
+{
+    [TestClass()]
+    public class MustBeTrueAttributeTests
+    {
+        [TestMethod()]
+        public void TrueIsValid()
+        {
+            var mustBetrueAttribute = new MustBeTrueAttribute();
+
+            var actual = mustBetrueAttribute.IsValid(true);
+
+            Assert.IsTrue(actual);
+        }
+
+        [TestMethod()]
+        public void NullIsInvalid()
+        {
+            var mustBetrueAttribute = new MustBeTrueAttribute();
+
+            var actual = mustBetrueAttribute.IsValid(null);
+            //return value != null && value is bool && (bool)value;
+
+            Assert.IsFalse(actual);
+        }
+
+        [TestMethod()]
+        public void StringIsInvalid()
+        {
+            var mustBetrueAttribute = new MustBeTrueAttribute();
+
+            var actual = mustBetrueAttribute.IsValid("string");
+
+            Assert.IsFalse(actual);
+        }
+
+        [TestMethod()]
+        public void FalseIsInvalid()
+        {
+            var mustBetrueAttribute = new MustBeTrueAttribute();
+
+            var actual = mustBetrueAttribute.IsValid(false);
+
+            Assert.IsFalse(actual);
+        }
+
+        [TestMethod()]
+        public void FormatErrorMessageTest()
+        {
+            Assert.Fail();
+        }
+
+        [TestMethod()]
+        public void GetClientValidationRulesTest()
+        {
+            Assert.Fail();
+        }
+    }
+}
